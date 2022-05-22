f0f0df90dbc34aad5d376de55435a38b88149e9b
Maria del Mar Cegarra <maria.cegarra2303@gmail.com>
2022-05-22 23:44:01 +0200

    Edited user routers

--- src/server/routers/UserRouter.js
+++ src/server/routers/UserRouter.js
@@ -2,11 +2,14 @@ const express = require("express");
 const multer = require("multer");
 const path = require("path");
 const { loginUser, registerUser } = require("../controllers/UserControllers");
+const { getUsers } = require("../controllers/UsersControllers");
+const auth = require("../middlewares/auth");
 
 const userRouter = express.Router();
 const uploads = multer({ dest: path.join("uploads", "images") });
 
 userRouter.post("/login", loginUser);
 userRouter.post("/register", uploads.single("image"), registerUser);
+userRouter.get("/users", auth, getUsers);
 
 module.exports = userRouter;
